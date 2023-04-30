4ce5fc0f8f0e919818715007c6604a94305334c0
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-04-30 23:07:28 +0700

    Add files via upload

---
+++ Basic/Form/Form get.php
@@ -0,0 +1,10 @@
+<html>
+  <body>
+<form action="/" method="get"> Name: <input type="text" name="name">
+<br> 
+E-mail: <input type="text" name="email">
+<br> 
+<input type="submit">
+</form>
+</body>
+</html>
