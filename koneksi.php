474938d35b4a14cf1af3815213e17cfcca60d1fd
Stevanus Andika <84663886+StevanusAndika@users.noreply.github.com>
2023-05-01 00:11:59 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$server="localhost";
-$user="root";
-$password=" ";
-$database="dbuploadfile"";
- //buat koneksi
-$koneksi=mysqli_connect($server,$user,$password,$database)or die(mysqli_error($koneksi));
-
-?>
